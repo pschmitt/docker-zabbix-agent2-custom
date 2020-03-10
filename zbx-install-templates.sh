4c0474b07f123ffb3a908fded09fb4dc100bc5d8
Philipp Schmitt <philipp@schmitt.co>
2020-03-10 14:10:07 +0100

    Use another dir than /etc/sudoers.d to allow it to be overridden at runtime

--- zbx-install-templates.sh
+++ zbx-install-templates.sh
@@ -4,11 +4,21 @@ usage() {
   echo "Usage: $(basename "$0") GIT_URL..."
 }
 
+patch_sudo_config() {
+  local sudoers_dir="${1:-/etc/sudoers.d_static}"
+  local line="#includedir ${sudoers_dir}"
+  if ! grep -q "^${line}$" /etc/sudoers
+  then
+    echo "$line" >> /etc/sudoers
+  fi
+}
+
 install_template() {
   local docker_config
   local file
   local filename
   local tmpdir=/git/_pkg
+  local sudoers_dir=/etc/sudoers.d_static
   local zbx_conf_dir=/etc/zabbix/zabbix_agentd.d_static
 
   echo "Installing $1"
@@ -38,21 +48,27 @@ install_template() {
   else
     mv -i "$tmpdir"/zabbix_agentd.conf.d/*.conf "$zbx_conf_dir"
   fi
+
+  # Scripts
   mv -i "$tmpdir"/zbx-*.sh /usr/local/bin
+
   # Remove the .docker extension
   # Otherwise the sudoers files will get ignored
+  mkdir -p "$sudoers_dir"
   for file in "$tmpdir"/sudoers.d/*.docker
   do
     filename="$(basename "$file" .docker)"
-    mv -i "$file" "/etc/sudoers.d/${filename}"
+    # mv -i "$file" "/etc/sudoers.d/${filename}"
+    mv -i "$file" "${sudoers_dir}/${filename}"
   done
 
   chown -R zabbix "$zbx_conf_dir"
 
   chmod +x /usr/local/bin/*
 
-  chown -R root:root /etc/sudoers.d
-  chmod 600 /etc/sudoers.d/*
+  patch_sudo_config "$sudoers_dir"
+  chown -R root:root "$sudoers_dir"
+  chmod 600 "${sudoers_dir}"/*
 
   rm -rf "$tmpdir"
 }
